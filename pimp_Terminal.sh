9b0253fe42b23db3ad095b0e26a62f88a76e2184
Michelle <172150370+Michellesdreamplace@users.noreply.github.com>
2024-08-30 15:30:43 +0200

    update

--- pimp_Terminal.sh
+++ pimp_Terminal.sh
@@ -1,52 +1,61 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGREY='\033[0;37m'
+NORMAL='\033[0;39m'
+
 BENUTZER=$USER
 HOMEVERZEICHNIS=$HOME
 MYSHELL=$SHELL
-echo "Hallo $BENUTZER"
-echo "Dein Home-Verzeichnis: $HOMEVERZEICHNIS"
-echo "Deine Shell: $MYSHELL"
-echo "************************************************************"
-echo "    installiere git (falls nicht vorhanden) ..."
-echo "    ... um Repositories klonen zu können ..."
-echo " ..."
+echo "$PURPLE Hallo $BENUTZER"
+echo "$PURPLE Dein Home-Verzeichnis: $HOMEVERZEICHNIS"
+echo "$PURPLE Deine Shell: $MYSHELL"
+echo "$GREEN ************************************************************"
+echo "$GREEN     installiere git (falls nicht vorhanden) ..."
+echo "$GREEN     ... um Repositories klonen zu können ..."
+echo "$GREEN  ... $NORMAL"
 sudo apt install git -y
-echo "************************************************************"
-echo "    installiere tar (falls nicht vorhanden) ..."
-echo "    ... um Dateien entpacken zu können ..."
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     installiere tar (falls nicht vorhanden) ..."
+echo "$GREEN     ... um Dateien entpacken zu können ..."
+echo "$GREEN  ... $NORMAL"
 sudo apt install tar -y
-echo "************************************************************"
-echo "    installiere neofetch (falls nicht vorhanden) ..."
-echo "    "
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     installiere neofetch (falls nicht vorhanden) ..."
+echo "$GREEN     "
+echo "$GREEN  ... $NORMAL"
 sudo apt install neofetch -y
-echo "************************************************************"
-echo "    installiere screenfetch (falls nicht vorhanden) ..."
-echo "    "
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     installiere screenfetch (falls nicht vorhanden) ..."
+echo "$GREEN     "
+echo "$GREEN  ... $NORMAL"
 sudo apt install screenfetch -y
-echo "************************************************************"
-echo "    klone Dateien zum Terminal pimpen von GitHub ..."
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     klone Dateien zum Terminal pimpen von GitHub ..."
+echo "$GREEN  ... $NORMAL"
 sudo git clone https://github.com/Michellesdreamplace/DEB_Terminal_pimpen.git $HOMEVERZEICHNIS/Downloads/MDP_DEB_Terminal_pimpen/
-echo "************************************************************"
-echo "    erstelle Sicherungskopie deiner bisherigen .bashrc ..."
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     erstelle Sicherungskopie deiner bisherigen .bashrc ..."
+echo "$GREEN  ... $NORMAL"
 
 # ---------- WIP ----------
 
-echo "************************************************************"
-echo "    entpacke Datei in dein persönlichen Ordner ..."
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     entpacke Datei in dein persönlichen Ordner ..."
+echo "$GREEN  ... $NORMAL"
 sudo tar -xzf $HOMEVERZEICHNIS/Downloads/MDP_DEB_Terminal_pimpen/Terminal_pimpen.tar.gz -C $HOMEVERZEICHNIS/
-echo "************************************************************"
-echo "    aufräumen ..."
-echo " ..."
+echo "$GREEN ************************************************************"
+echo "$GREEN     aufräumen ..."
+echo "$GREEN  ... $NORMAL"
 sudo rm -r $HOMEVERZEICHNIS/Downloads/MDP_DEB_Terminal_pimpen
-echo "************************************************************"
-echo " "
-echo "    Fertig ..."
-echo " "
-echo "  Viel Spaß mit deinem gepimpten Terminal :)"
-echo " "
-echo "************************************************************"
+echo "$CYAN ************************************************************"
+echo "$CYAN  "
+echo "$CYAN     Fertig ..."
+echo "$CYAN  "
+echo "$CYAN   Viel Spaß mit deinem gepimpten Terminal :)"
+echo "$CYAN  "
+echo "$CYAN ************************************************************ $NORMAL"
